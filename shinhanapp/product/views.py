804056881267922718b2624e6693efe5b8270c98
yjh0816 <2543kt@naver.com>
2023-01-17 12:39:23 +0900

    feat: Sort product order by descending

--- shinhanapp/product/views.py
+++ shinhanapp/product/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 # Create your views here.
 def main(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
     # products = Product.objects.filter(title__contains='test')
 
     return render(request,'product.html', {'products': products})
